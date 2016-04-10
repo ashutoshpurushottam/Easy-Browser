304820e44c2289498d1190e2512bb666fd6608ae
ashutoshpurushottam <apurushottam@icloud.com>
2016-04-10 09:40:45 +0530

    Set navigationcontrolloer add alert action for chosing apple.com

--- Easy-Browser/ViewController.swift
+++ Easy-Browser/ViewController.swift
@@ -29,7 +29,24 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.loadRequest(NSURLRequest(URL: url))
         // allows users to swipe from the left or right edge to move backward or forward in their web browsing
         webView.allowsBackForwardNavigationGestures = true
+        // add button to the navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
     }
+    
+    func openTapped()
+    {
+        let uc = UIAlertController(title: "Open Page..", message: nil, preferredStyle: .ActionSheet)
+        uc.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        uc.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(uc, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction)
+    {
+        let url = NSURL(string: "https://" + action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
 
 
 
